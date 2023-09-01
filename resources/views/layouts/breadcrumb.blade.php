3dd7e7bc18cf60ef2c59bc0c500c23830e714e08
MuhAzmi <azmi2793@gmail.com>
2023-09-01 08:27:10 +0700

    add breadcrumb to default template

---
+++ resources/views/layouts/breadcrumb.blade.php
@@ -0,0 +1,8 @@
+<div id="breadcrumb" class="mt-3">
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">{{ $page_title }}</li>
+    </ol>
+  </nav>
+</div>
