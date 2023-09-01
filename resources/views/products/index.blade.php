4db7ac77be097502f8df8c9d47bb6c8102fd7608
MuhAzmi <azmi2793@gmail.com>
2023-09-01 08:27:25 +0700

    change product index layout

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,4 +2,30 @@
 
 @section('content')
   <h1>{{$page_title}}</h1>
+
+  <p>
+    <a href="{{ url('product/create') }}" class="btn btn-primary">
+      <i class="fa-solid fa-plus"></i> Tambah Produk
+    </a>
+  </p>
+
+  <div class="content">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <td>No</td>
+          <td>Product Name</td>
+          <td>Description</td>
+          <td>Category</td>
+          <td>Qty</td>
+          <td>Price</td>
+          <td>Image</td>
+          <td>#</td>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  </div>
 @endsection
